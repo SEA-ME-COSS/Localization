69085648749793f7d1b4bbf29e05e4cfe2ba979f
lalywr2000 <12190450@inha.edu>
2024-05-02 16:17:30 +0200

    Add map for rviz

---
+++ local_ws/src/map_publisher/launch/map_publisher.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import launch_ros.actions
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    map_server_cmd = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': "/home/ha/Desktop/Localization/local_ws/src/map_publisher/map/map.yaml"}])
+
+    lifecycle_nodes = ['map_server']
+    use_sim_time = True
+    autostart = True
+
+    start_lifecycle_manager_cmd = launch_ros.actions.Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager',
+            output='screen',
+            emulate_tty=True,
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}])
+
+    ld.add_action(map_server_cmd)
+    ld.add_action(start_lifecycle_manager_cmd)
+
+    return ld

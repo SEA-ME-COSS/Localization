72ec43f1afd30bd01397c40cca3e1810a69868d3
lalywr2000 <12190450@inha.edu>
2024-06-05 16:40:11 +0200

    Fix can frame bug

--- rpi_ws/src/sim_receiver/sim_receiver/sim_receiver.py
+++ rpi_ws/src/sim_receiver/sim_receiver/sim_receiver.py
@@ -36,10 +36,10 @@ class SimulationReceiver(Node):
         elif throttle > 1.0:
             throttle = 1.0
 
-        msg = can.Message(arbitration_id=0, data=[(steering > 0), int(abs(steering)), int((steering % 1) * 100), 0])
+        msg = can.Message(arbitration_id=0, data=[(steering > 0), int(abs(steering)), int((abs(steering) % 1) * 100), 0])
         self.bus.send(msg)
 
-        msg = can.Message(arbitration_id=1, data=[(throttle < 0), int(abs(throttle)), int((throttle % 1) * 100), 0])
+        msg = can.Message(arbitration_id=1, data=[(throttle < 0), int(abs(throttle)), int((abs(throttle) % 1) * 100), 0])
         self.bus.send(msg)
 
         # self.get_logger().info('')
@@ -59,13 +59,13 @@ class SimulationReceiver(Node):
         if yaw_degrees < 0:
             yaw_degrees += 360
 
-        msg = can.Message(arbitration_id=2, data=[(x_pos < 0), int(abs(x_pos)), int((x_pos % 1) * 100), 0])
+        msg = can.Message(arbitration_id=2, data=[(x_pos < 0), int(abs(x_pos)), int((abs(x_pos) % 1) * 100), 0])
         self.bus.send(msg)
 
-        msg = can.Message(arbitration_id=3, data=[(y_pos < 0), int(abs(y_pos)), int((y_pos % 1) * 100), 0])
+        msg = can.Message(arbitration_id=3, data=[(y_pos < 0), int(abs(y_pos)), int((abs(y_pos) % 1) * 100), 0])
         self.bus.send(msg)
 
-        msg = can.Message(arbitration_id=4, data=[int(abs(yaw_degrees)), int((yaw_degrees % 1) * 100), 0, 0])
+        msg = can.Message(arbitration_id=4, data=[int(abs(yaw_degrees)), int((abs(yaw_degrees) % 1) * 100), 0, 0])
         self.bus.send(msg)
 
         # self.get_logger().info('')
